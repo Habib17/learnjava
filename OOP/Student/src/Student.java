25e186d39c0fb6ecc020658f0c663648352fd7e1
Muhamad Habib <muhamadhabib1000@gmail.com>
2018-09-09 10:09:57 +0700

    learn constructor on java

--- OOP/Student/src/Student.java
+++ OOP/Student/src/Student.java
@@ -51,7 +51,14 @@ public class Student {
     */
     Student student = new Student();
     Student student1 = new Student();
+    //access constructor use name
+    Student student3 = new Student("Adi");
     student.name ="Fulan";
+    // three ways of instatiation
+    Student s1 = new Student("Adi");
+    Student s2 = new Student(20);
+    Student s3 = new Student("Ali", 21);
+
     /*
     error
     student.tall =167;
@@ -61,7 +68,59 @@ public class Student {
     student.data();  //print method info with local variable name replace by fulan name
 
     
-    
+    //check if an object is an instance of a class
+    //instanceof if there are object can use  ==
+    if(student instanceof Student){
+        System.out.println("Mereka adalah instance");
+    }
+    //calling constructor
+    new Student();
     }
     
+ /*
+Constuctors are special methods
+that have the same name
+as the class name
+Constuctors don’t
+return any values
+    */
+    public Student() {
+        System.out.println("Constructor dipanggil");
+            /*Constructors are useful
+        for initializing variables
+         */
+         name = "nama constructor";
+	}
+    //Constructors can have parameters
+    public Student(String name) {
+		this.name = name;
+                //call constructor
+                //this(name, 20);
+
+	}
+       //constructor overloaded
+    public Student(int age) {
+		this.age = age;
+	}
+
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+
 }
+/*
+We don’t have to import classes
+located in java.lang package
+
+Classes in java.lang package:
+Object
+System
+String
+Math
+Wrapper classes
+Exception
+Thread
+*/
